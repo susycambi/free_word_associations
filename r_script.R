a5ac46308ce4ab57ff65c9986b690a4bea7ccfa7
Susanne Cambi <62621674+susycambi@users.noreply.github.com>
2023-06-15 17:30:54 +0200

    Update r_script.R

--- r_script.R
+++ r_script.R
@@ -1,19 +1,18 @@
+### this small R script is for computing some basic statistics on the pivoted results
+### 
+
 setwd("/Users/susannecambi/Documents/neuroscience_master/Noce_internship/dev/FWA")
 data <- read.csv("pivoted_results.csv")
 str(data)
 
-# only keep the rows (cue words) for which there are less than 6 zero values (do 6 out of 12 subjects have the file)
-# only remove the "flat" cues
+# only remove the "flat" cues (keeping the rows for which there are less than 12 0 values)
 data <- data[rowSums(data == 0) < 12, ]
 
-
+# mean count of associations per cue/word
 data[data == 0] <- NA
 data$mean_count <- rowMeans(data[ , 14:25], na.rm = TRUE)
 
-# mean count by subject
-# number of 0 / Na by subject
-
-
+# mean count of associations per subject
 mean_n_associations <- colMeans(data[ , 14:25], na.rm = T)
 n_missing <- colSums(is.na(data[ , 14:25]))
 percent_missing <- (colSums(is.na(data[ , 14:25]))/97)*100
